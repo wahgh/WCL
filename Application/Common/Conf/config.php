c8b5796121b688f366c6c6f333a86c9f21738aa7
wah <546290057@qq.com>
2016-04-02 17:07:14 +0800

    添加四大模块的模板

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -1,4 +1,21 @@
 <?php
 return array(
-	//'配置项'=>'配置值'
+    /* 数据库设置 */
+    'DB_TYPE' => 'mysql', // 数据库类型
+    'DB_HOST' => '127.0.0.1', // 服务器地址
+    'DB_NAME' => 'wcl', // 数据库名
+    'DB_USER' => 'root', // 用户名
+    'DB_PWD' => '', // 密码
+    'DB_PORT' => '3306', // 端口
+    'DB_PREFIX' => 'wcl_', // 数据库表前缀
+    'LAYOUT_ON' => true, // 是否启用布局
+    'URL_MODEL' => 2, // URL访问模式,可选参数0、1、2、3,代表以下四种模式：
+    // 0 (普通模式); 1 (PATHINFO 模式); 2 (REWRITE  模式); 3 (兼容模式)  默认为PATHINFO 模式
+    'URL_HTML_SUFFIX' => '', // URL伪静态后缀设置
+    'TMPL_PARSE_STRING' => [
+        'WEB_NAME' => '职来职往',
+        'IMAGE_PATH' => '/Public/images',
+        'UPLOAD_IMAGE_PATH' => '/upload/images'
+    ],
+    //'配置项'=>'配置值'
 );
